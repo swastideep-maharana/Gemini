1b8cbd3bec494c368175fbe6fcefa7f49853b819
swastideep maharana <swastideep67maharana@gmail.com>
2025-08-05 21:02:43 +0530

    #fix: disabled no-explicit-any and unescaped-entities rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+ 
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
